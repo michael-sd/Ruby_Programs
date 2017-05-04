97c37a69cff182d7462e7df9d994c185c0afd35c
Michael Santo Domingo <msd2802@gmail.com>
2017-05-04 01:25:05 +0000

    Built leaves of tree

--- tree_traversal.rb
+++ tree_traversal.rb
@@ -6,5 +6,10 @@ class Tree
     @children = children
   end
 
+# The "Leaves" of the tree, elements that have no children
+deep_fifth_node = Tree.new(5, [])
+eleventh_node = Tree.new(11, [])
+fourth_node = Tree.new(4, [])
+
 
 end
