71908f16e66c5e23709a9a62322db7c37c494308
Michael Santo Domingo <msd2802@gmail.com>
2017-05-04 01:35:08 +0000

    Added tree branches and trunk

--- tree_traversal.rb
+++ tree_traversal.rb
@@ -6,10 +6,18 @@ class Tree
     @children = children
   end
 
+end
+
 # The "Leaves" of the tree, elements that have no children
 deep_fifth_node = Tree.new(5, [])
 eleventh_node = Tree.new(11, [])
 fourth_node = Tree.new(4, [])
 
+# The "Branches" of the tree.
+ninth_node = Tree.new(9, [fourth_node])
+sixth_node = Tree.new(6, [deep_fifth_node, eleventh_node])
+seventh_node = Tree.new(7, [sixth_node])
+shallow_fifth_node = Tree.new(5, [ninth_node])
 
-end
+# The "Trunk" of the tree.
+trunk = Tree.new(2, [seventh_node, shallow_fifth_node])
