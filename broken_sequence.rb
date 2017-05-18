7e661ea09ddc79881d353479aa09f57d3780bd6f
Michael Santo Domingo <msd2802@gmail.com>
2017-05-18 06:26:29 +0000

    Broken Sequence

---
+++ broken_sequence.rb
@@ -0,0 +1,23 @@
+def find_missing_number(sequence)
+  return 0 if sequence == ''
+  a = sequence.split(" ").map {|i| i.to_i}.sort
+  return 1 if a.first != 1
+  return (a.first..a.last).reduce(:+) - a.reduce(:+)
+end
+
+
+# Description:
+
+# You have a sequence of positive numbers starting with 1, but one number is missing!
+
+# Find out the missing number; if the sequence is not broken, you should return 0. Each sequence always increments by 1.
+
+# In short: an invalid sequence must return 1, an already complete (or empty) sequence must return 0; otherwise return the missing element.
+
+# Note that the sequence may be shuffled.
+
+# E.g.
+
+# find_missing_number("1 2 3 5") # returns 4
+# find_missing_number("1 2 3 4") # returns 0
+# find_missing_number("5 1 3 4") # returns 2
