f8ee6b5f6d5d883a5c0caff5b8bc0153dacd3bff
Michael Santo Domingo <msd2802@gmail.com>
2017-05-19 00:13:02 +0000

    remove_dupllicates

---
+++ remove_duplicates.rb
@@ -0,0 +1,10 @@
+# Define the unique method that removes duplicates
+def unique(integers)
+  array = []
+  integers.each {|n| array << n if array.include?(n) == false}
+  puts array
+end
+
+unique([])
+unique([5, 2, 1, 3])
+unique([1, 5, 2, 0, 2, -3, 1, 10])
